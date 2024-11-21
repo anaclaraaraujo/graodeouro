72b92b2e735b44065dcf428ca1787ca344a7219e
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-11-21 16:47:33 -0300

    feat: first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: ${(props) => props.theme.white};
+    color: ${(props) => props.theme['gray-800']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 1.6rem;
+  }
+
+  button {
+    border: none;
+    cursor: pointer;
+  }
+`;
