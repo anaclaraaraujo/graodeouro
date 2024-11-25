e8d5d11fcef8b3e9d5f603ee6261378d938cc956
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2024-11-25 18:01:23 -0300

    feat: create checkout page

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,5 +1,205 @@
+import { Fragment, useState } from 'react';
+import { Bank, CreditCard, CurrencyDollar, MapPin, Money, Trash } from '@phosphor-icons/react';
+import { TextInput } from '../../components/TextInput';
+import { Radio } from '../../components/Radio';
+import {
+  AddressContainer,
+  AddressForm,
+  AddressHeading,
+  CartTotal,
+  CartTotalInfo,
+  CheckoutButton,
+  Coffee,
+  CoffeeInfo,
+  Container,
+  InfoContainer,
+  PaymentContainer,
+  PaymentHeading,
+  PaymentOptions,
+} from './styles';
+
+import img from '/images/coffees/cafe_com_leite.png';
+import { QuantityInput } from '../../components/QuantityInput';
+import { useCart } from '../../contexts/CartContext';
+
 export function Checkout() {
+  const [quantity, setQuantity] = useState(1);
+  const [isItemAdded, setIsItemAdded] = useState(false);
+  const { addItem } = useCart();
+  function incrementQuantity() {
+    setQuantity((state) => state + 1);
+  }
+
+  function decrementQuantity() {
+    if (quantity > 1) {
+      setQuantity((state) => state - 1);
+    }
+  }
+
+  // Mark item as added to cart and reset quantity
+  function handleAddItem() {
+    addItem();
+    setIsItemAdded(true);
+    setQuantity(1);
+  }
+
   return (
-    <h1>aa</h1>
+    <Container>
+      <InfoContainer>
+        <h2>Complete seu pedido</h2>
+
+        <form id="order">
+          <AddressContainer>
+            <AddressHeading>
+              <MapPin size={22} />
+
+              <div>
+                <span>Endereço de Entrega</span>
+
+                <p>Informe o endereço onde deseja receber o seu pedido</p>
+              </div>
+            </AddressHeading>
+
+            <AddressForm>
+              <TextInput
+                placeholder="CEP"
+                type="number"
+                containerProps={{ style: { gridArea: 'cep' } }}
+              />
+
+              <TextInput
+                placeholder="Rua"
+                containerProps={{ style: { gridArea: 'street' } }}
+              />
+
+              <TextInput
+                placeholder="Número"
+                containerProps={{ style: { gridArea: 'number' } }}
+              />
+
+              <TextInput
+                placeholder="Complemento"
+                optional
+                containerProps={{ style: { gridArea: 'fullAddress' } }}
+              />
+
+              <TextInput
+                placeholder="Bairro"
+                containerProps={{ style: { gridArea: 'neighborhood' } }}
+              />
+
+              <TextInput
+                placeholder="Cidade"
+                containerProps={{ style: { gridArea: 'city' } }}
+              />
+
+              <TextInput
+                placeholder="UF"
+                maxLength={2}
+                containerProps={{ style: { gridArea: 'state' } }}
+              />
+            </AddressForm>
+          </AddressContainer>
+
+          <PaymentContainer>
+            <PaymentHeading>
+              <CurrencyDollar size={22} />
+
+              <div>
+                <span>Pagamento</span>
+
+                <p>
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </p>
+              </div>
+            </PaymentHeading>
+
+            <PaymentOptions>
+              <div>
+                <Radio
+                  $isSelected={true}
+                  value="credit"
+                >
+                  <CreditCard size={16} />
+                  <span>Cartão de crédito</span>
+                </Radio>
+
+                <Radio
+                  $isSelected={true}
+                  value="debit"
+                >
+                  <Bank size={16} />
+                  <span>Cartão de débito</span>
+                </Radio>
+
+                <Radio
+                  $isSelected={true}
+                  value="cash"
+                >
+                  <Money size={16} />
+                  <span>Dinheiro</span>
+                </Radio>
+              </div>
+            </PaymentOptions>
+          </PaymentContainer>
+        </form>
+      </InfoContainer>
+
+      <InfoContainer>
+        <h2>Cafés selecionados</h2>
+
+        <CartTotal>
+          <Fragment>
+            <Coffee>
+              <div>
+                <img src={img} alt="Expresso Tradicional" />
+
+                <div>
+                  <span>Expresso Tradicional</span>
+
+                  <CoffeeInfo>
+                    <QuantityInput
+                      quantity={quantity}
+                      incrementQuantity={incrementQuantity}
+                      decrementQuantity={decrementQuantity}
+                    />
+                    <button>
+                      <Trash />
+                      <span>Remover</span>
+                    </button>
+                  </CoffeeInfo>
+                </div>
+              </div>
+
+              <aside>R$ 9,90</aside>
+            </Coffee>
+
+            <span />
+          </Fragment>
+
+          <CartTotalInfo>
+            <div>
+              <span>Total de itens</span>
+              <span> R$ 9.90 </span>
+            </div>
+
+            <div>
+              <span>Entrega</span>
+              <span> R$ 9.90 </span>
+            </div>
+
+            <div>
+              <span>Total</span>
+              <span> R$ 9.90 </span>
+            </div>
+          </CartTotalInfo>
+
+          <CheckoutButton type="submit" form="order">
+            Confirmar pedido
+          </CheckoutButton>
+        </CartTotal>
+      </InfoContainer>
+    </Container>
   )
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,220 @@
+import styled from 'styled-components';
+
+export const Container = styled.label`
+  max-width: 112rem;
+  margin: 0 auto;
+
+  margin-top: 10.4rem;
+
+  padding: 4rem 2rem;
+  
+  display: flex;
+  gap: 3.2rem;
+`;
+
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+
+  h2 {
+    ${(props) => props.theme.fonts.titleXS};
+    color: ${(props) => props.theme.colors['gray-950']};
+  }
+
+  > form {
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
+  }
+`;
+
+export const FormsContainer = styled.div`
+  padding: 4rem;
+  border-radius: .6rem;
+  background-color: ${(props) => props.theme.colors['gray-300']};
+  width: 100%;
+  min-width: 64rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
+export const AddressContainer = styled(FormsContainer)``;
+
+export const Heading = styled.div`
+  display: flex;
+  gap: 8px;
+
+  div {
+    span {
+      color: ${(props) => props.theme.colors['gray-900']};
+    }
+
+    p {
+      ${(props) => props.theme.fonts.textS};
+    }
+  }
+`;
+
+export const AddressHeading = styled(Heading)`
+  svg {
+    color: ${(props) => props.theme.colors['yellow-700']};
+  }
+`;
+
+export const AddressForm = styled.div`
+  display: grid;
+  grid-template-areas:
+    'cep . .'
+    'street street street'
+    'number fullAddress fullAddress'
+    'neighborhood city state';
+  grid-template-columns: 20rem 1fr 6rem;
+  grid-gap: 1.6rem 1.2rem;
+`;
+
+export const PaymentContainer = styled(FormsContainer)``
+
+export const PaymentHeading = styled(Heading)`
+  svg {
+    color: ${(props) => props.theme.colors['purple-500']};
+  }
+`;
+
+export const PaymentOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: .8rem;
+
+  > div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1.2rem;
+  }
+`;
+
+export const Coffee = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  > div {
+    > img {
+      width: 6.4rem;
+      height: 6.4rem;
+    }
+
+    display: flex;
+    align-items: stretch;
+    gap: 2rem;
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+  }
+
+  > aside {
+    font-weight: bold;
+  }
+`;
+
+export const CoffeeInfo = styled.div`
+  display: flex;
+  gap: .8rem;
+
+  > button {
+    padding: 6px 8px;
+    background-color: ${(props) => props.theme.colors['gray-500']};
+    border-radius: .6rem;
+    display: flex;
+    align-items: center;
+    gap: .8rem;
+
+    transition: all 0.2s;
+
+    &:hover {
+      background-color: ${(props) => props.theme.colors['gray-600']};
+    }
+
+    > svg {
+      color: ${(props) => props.theme.colors['purple-500']};
+    }
+
+    > span {
+      ${(props) => props.theme.fonts.buttonM};
+      text-transform: uppercase;
+      color: ${(props) => props.theme.colors['gray-800']};
+    }
+  }
+`;
+
+export const CartTotal = styled.div`
+  padding: 4rem;
+  border-radius: .6rem 3.6rem;
+  background-color: ${(props) => props.theme.colors['gray-300']};
+  width: 100%;
+  min-width: 44.8rem;
+
+  > span {
+    display: block;
+    height: 1px;
+    background-color: ${(props) => props.theme.colors['gray-500']};
+    margin: 2.4rem 0;
+  }
+`
+
+export const CartTotalInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    span:first-child {
+      ${(props) => props.theme.fonts.textS};
+    }
+
+    span:last-child {
+      ${(props) => props.theme.fonts.textM};
+    }
+  }
+
+  div:last-child {
+    span {
+      ${(props) => props.theme.fonts.textL};
+      font-weight: bold;
+    }
+  }
+`
+
+export const CheckoutButton = styled.button`
+  margin-top: 2.4rem;
+  width: 100%;
+  padding: 1.2rem;
+  text-transform: uppercase;
+
+  ${(props) => props.theme.fonts.buttonG};
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors['yellow-500']};
+
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors['yellow-700']};
+  }
+
+  border-radius: 6px;
+`;
+
+export const PaymentErrorMessage = styled.p`
+  ${(props) => props.theme.fonts.textXS};
+  font-weight: 400;
+  color: red;
+`;
